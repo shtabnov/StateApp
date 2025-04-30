be2892ba106d2ec71f5485cf8736241c628a2073
shtabnov <shtabnovan@redu.local>
2025-04-30 08:13:43 +0500

    add compareTo

--- State.cs
+++ State.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace StateApp
 {
-    public abstract class State
+    public abstract class State : IComparable<State>
     {
         public string Name { get; set; }
         public DateTime FoundationDate { get; set; }
+        public int Population { get; set; }
 
-        protected State(string name, DateTime foundationDate)
+        protected State(string name, DateTime foundationDate, int population)
         {
             Name = name;
             FoundationDate = foundationDate;
+            Population = population;
         }
 
         public abstract void DisplayInfo();
@@ -25,5 +28,15 @@ namespace StateApp
 
             return age;
         }
+
+
+        public int CompareTo(State? other)
+
+        {
+            if (other == null) return 1;
+            return this.Population.CompareTo(other.Population);
+        }
+
+
     }
 }
