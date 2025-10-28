08189016fdeaeecd6e3be3f5acd09f95ce4aa475
shtabnov <second88@bk.ru>
2025-10-28 17:30:31 +0500

    add LeaderName, comparators and ICloneable

--- State.cs
+++ State.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 
 namespace StateApp
 {
-    public abstract class State : IComparable<State>
+    public abstract class State : IComparable<State>, ICloneable
     {
         public string Name { get; set; }
         public DateTime FoundationDate { get; set; }
         public int Population { get; set; }
+        public string LeaderName { get; set; }
 
-        protected State(string name, DateTime foundationDate, int population)
+        protected State(string name, DateTime foundationDate, int population, string leaderName)
         {
             Name = name;
             FoundationDate = foundationDate;
             Population = population;
+            LeaderName = leaderName;
         }
 
         public abstract void DisplayInfo();
@@ -29,14 +31,18 @@ namespace StateApp
             return age;
         }
 
-
         public int CompareTo(State? other)
-
         {
             if (other == null) return 1;
             return this.Population.CompareTo(other.Population);
         }
 
+        public static IComparer<State> SortByFoundationDate =>
+            Comparer<State>.Create((a, b) => a.FoundationDate.CompareTo(b.FoundationDate));
 
+        public static IComparer<State> SortByLeaderName =>
+            Comparer<State>.Create((a, b) => string.Compare(a.LeaderName, b.LeaderName, StringComparison.CurrentCulture));
+
+        public virtual object Clone() => MemberwiseClone();
     }
 }
